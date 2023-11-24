b95052cd5230189c5e1d6cce515bdee70ffecd00
Chema <mrjark7@gmail.com>
2023-11-24 19:59:00 +0100

    added zustand

---
+++ midudev_/02_spotify_clone/src/components/CardPlayBotton.jsx
@@ -0,0 +1,9 @@
+import {Play} from "./Player"
+
+export function CardPlayBotton({ id }) {
+  return (
+    <div className="card-play-botton rounded-full p-3 bg-green-500">
+      <Play />
+    </div>
+  )
+}
