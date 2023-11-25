0aedf81a3a49bd87db760f5652f59ec60ed59621
Chema <mrjark7@gmail.com>
2023-11-25 14:03:47 +0100

    resolved problem with syncro

--- midudev_/02_spotify_clone/src/store/playerStore.js
+++ midudev_/02_spotify_clone/src/store/playerStore.js
@@ -1,8 +1,11 @@
-import { create } from "zustand"
+// ESTADO GLOBAL
+import { create } from "zustand";
 
 export const usePlayerStore = create( ( set ) => ( {
   isPlaying: false,
-  currentMusic: { playlist: null, song: null, song: [] },
+  currentMusic: { playlist: null, song: null, songs: [] },
+  // volume: 1,
+  // setVolume: ( volume ) => set( { volume } ),
   setIsPlaying: ( isPlaying ) => set( { isPlaying } ),
-  setCurrentMusic: ( currentMusic ) => set( { currentMusic } )
+  setCurrentMusic: ( currentMusic ) => set( { currentMusic } ),
 } ) )
